cd48a8cde5af48433bb8cb4f5c4ba62fb296a451
Saja Elfaki <saja@meta.com>
2024-06-25 09:56:26 -0700

    setting up frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
